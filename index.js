5023ca2ae5fc2a72111dfe1b5976b6b3ed9991d3
bdevore17 <bdevore17@gmail.com>
2016-12-05 21:39:47 -0500

    changes to let's encrypt

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(request, response) {
   response.send("flynn test app");
 });
 
+app.get('/.well-known/acme-challenge/RiPIzfn4N13RzMvxwyZ59je-Crr2hep4uhuMpRAOV6k', function(request, response) {
+  response.send("RiPIzfn4N13RzMvxwyZ59je-Crr2hep4uhuMpRAOV6k.vw09nxnidQigwrCaf_GliJd0YEsPfYVZx6qltjohDUE");
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('running on port', port);
