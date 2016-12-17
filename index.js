1980c66c6b69e88a46b4a72d13e917b0984fdc3c
bdevore17 <bdevore17@gmail.com>
2016-12-16 21:25:54 -0500

    generalize for let's encrypt

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send("flynn test app");
+  response.send("server test app");
 });
 
-app.get('/.well-known/acme-challenge/RiPIzfn4N13RzMvxwyZ59je-Crr2hep4uhuMpRAOV6k', function(request, response) {
-  response.send("RiPIzfn4N13RzMvxwyZ59je-Crr2hep4uhuMpRAOV6k.vw09nxnidQigwrCaf_GliJd0YEsPfYVZx6qltjohDUE");
-});
+if (process.env.LETS_ENCRYPT_PATH && process.env.LETS_ENCRYPT_STRING) {
+  app.get(process.env.LETS_ENCRYPT_PATH, function(request, response) {
+    response.send(process.env.LETS_ENCRYPT_STRING);
+  });
+}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('running on port', port);
-})
+});
